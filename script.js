0f089f411416db2281f914fb08a8d9729582127f
ZoIman <maksymnagornyi@ukr.net>
2025-11-29 16:40:00 +0200

    ячс

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let productsArray = [];
 let xhr = new XMLHttpRequest();
 let url = "https://my-json-server.typicode.com/RobocodeSchool/marketplace";
 
-xhr.open('GET', url + '/products');
+xhr.open("GET", url + "/products");
 xhr.responseType = "json";
 xhr.onload = function () {
   productsArray = xhr.response;
   productsGrid.innerHTML = null;
-  productsArray.forEach(p => {
+  productsArray.forEach((p) => {
     productsArray.push(p);
     let pElem = document.createElement("div");
     pElem.classList.add("product");
@@ -23,87 +23,7 @@ xhr.onload = function () {
     productsGrid.append(pElem);
   });
 };
-// xhr.send();
 
-// function addProductToCart(id) {
-//   xhr.open("GET", `${url}/products/${id}`);
-//   xhr.responseType = "json";
-//   xhr.onload = function () {};
-// }
-
-// let cartProd = document.getElementById("cart-products");
-
-// let cart = [];
-// if (localStorage.getItem("cart")) {
-//   cart = JSON.parse(localStorage.getItem("cart"));
-//   drawCartProducts();
-// }
-
-// function addProductToCart(id) {
-//   let product = productsArray.find(function (p) {
-//     return p.id == id;
-//   });
-//   cart.push(product);
-//   drawCartProducts();
-//   localStorage.setItem("cart", JSON.stringify(cart));
-
-//   document.getElementById("cart-button").classList.add("active");
-//   setTimeout(function () {
-//     document.getElementById("cart-button").classList.remove("active");
-//   }, 500);
-// }
-
-// function drawCartProducts() {
-//   if (cart.length === 0) return cartProd.innerHTML = "cart is empty";
-//   cartProd.innerHTML = null;
-//   let sum = 0;
-//   cart.forEach(function (p) {
-//     cartProd.innerHTML += `
-//         <p><img src="${p.photo_url}"> ${p.name} |${p.price}$</p>
-//         <hr>
-
-//      `;
-//      sum += p.price;
-//   });
-//   cartProd.innerHTML += `
-//   <p>Total Price: ${sum}$</p>
-//   <button onclick="buyAll()">Buy all</button>`;
-// }
-
-// function buyAll(){
-//     cart = []
-//     cartProd.innerHTML = "Ваші гроші безслідно зникли, дякуємо за підтримку ЗСУ"
-//     localStorage.setItem("cart", "[]")
-// }
-
-// function openCart() {
-//   cartProd.classList.toggle("hide");
-// }
-// let productsGrid = document.getElementById('products-grid');
-// let productsArray = [];
-// let xhr = new XMLHttpRequest();
-// let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
-
-// xhr.open('GET',url + '/products');
-// xhr.responseType = 'json'
-// xhr.onload = function() {
-//     productsArray = xhr.response
-//     productsGrid.innerHTML = null;
-//     productsArray.forEach(p => {
-//         productsArray.push(p);
-//         let pElem = document.createElement('div');
-//         pElem.classList.add('product');
-//         pElem.innerHTML = `
-//             <h2 class='product-name'>${p.name}</h2>
-//             <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
-//             <p class='product-price'><b>Price: </b>${p.price}$</p>
-//             <p class='product-description'><b>Description: </b>${p.description}</p>
-//             <a href='userProfile.html?id=${p.author_id}'>Seller profile</a>
-//             <button onclick="addProductToCart(${p.id})">Buy</button>
-//         `;
-//         productsGrid.append(pElem);
-//     });
-// }
 xhr.send();
 
 function addProductToCart(id) {
@@ -111,8 +31,6 @@ function addProductToCart(id) {
   xhr.responseType = "json";
   xhr.onload = function () {};
 }
-
-// CART ----------------
 
 let cartProd = document.getElementById("cart-products");
 
